c26d8e4990b1e6526b3aed7a00a7fb92f193c0ca
John Spinella <jrspinella@users.noreply.github.com>
2024-04-20 11:02:44 -0400

    Remove outputs, fix readmd

--- outputs.tf
+++ outputs.tf
@@ -55,16 +55,6 @@ output "laws_storage_account_location" {
   value       = local.location
 }
 
-output "laws_private_link_scope_id" {
-  description = "LAWS Private Link ID"
-  value       = module.mod_ampls.*.azurerm_monitor_private_link_scope_id
-}
-
-output "laws_private_link_scope_name" {
-  description = "LAWS Private Link ID"
-  value       = module.mod_ampls.*.azurerm_monitor_private_link_scope_name
-}
-
 output "automation_account_id" {
   description = "Automation Account ID"
   value       = module.lz_management_resources.automation_account.id
